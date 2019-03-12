6bd1d09a074a544fc9cff60359ee33edd9b74325
AntoinePlumerault <antoine.plumerault@gmail.com>
2019-03-12 12:59:22 +0100

    Correction of a bug

--- 3_TRANFER/code-tf/main_tf-2.0.py
+++ 3_TRANFER/code-tf/main_tf-2.0.py
@@ -61,6 +61,69 @@ def main(argv):
 
     # Get the filenames and label of our data
     image_filenames = []
+    image_labels = []# -*- coding: utf-8 -*
+from __future__ import absolute_import, division, print_function
+
+import os
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow.keras as K
+
+from absl import app, flags, logging
+from operator import itemgetter
+
+# default project structure
+
+# project
+#   +--code-tf
+#   |   +--main.py
+#   |   +--README_tf.md
+#   |
+#   +--data
+#       +--coast
+#       |   +--images.jpg
+#       |   +--...
+#       +--forest--
+#       |   +--images.jpg
+#       |   +--.
+#       +--highway
+#           +--image.jpg
+#           +--...
+
+# NB: vous pouvez faire un lien dynamique vers les données:
+#           cd project
+#           ln -s /path/to/data data
+
+# ======================== Define some usefull flags ===========================
+
+FLAGS = flags.FLAGS
+flags.DEFINE_integer('batch_size', 10, '')
+flags.DEFINE_float('learning_rate', 0.001, '')
+flags.DEFINE_integer('initial_step', 0, '')
+flags.DEFINE_integer('final_step', -1, 'set to `-1` to train indefinitely')
+flags.DEFINE_integer('info_freq', 1, '')
+flags.DEFINE_integer('valid_freq', 5, '')
+flags.DEFINE_string('data_dir', '../data', '')
+flags.DEFINE_bool('fine_tune', False, '')
+
+# example:
+# >> python main.py --batch_size=16 --final_step=20 --info_freq=1
+#
+# you can try with larger batch_size or more steps for better performances (but it's slower)
+
+# ======================== Load a pre-trained network ==========================
+
+def main(argv):
+
+    np.random.seed(43) # to make the results reproductible
+    tf.random.set_seed(42) # to make the results reproductible 
+    logging.set_verbosity(logging.INFO)
+
+    # ======================= Read target problem data =========================
+
+    # Get the filenames and label of our data
+    image_filenames = []
     image_labels = []
     for label, category in enumerate(['coast', 'forest', 'highway']):
         image_names = os.listdir(os.path.join(FLAGS.data_dir, category))
@@ -120,7 +183,7 @@ def main(argv):
     test_dataset = make_iterator(test_image_filenames, test_image_labels,
         batch_size=FLAGS.batch_size)
 
-    # ====================== Do the actual transfer learning =======================
+    # ====================== Do the actual transfer learning ===================
 
     # Define our model with keras model subclassing
 
@@ -132,7 +195,7 @@ def main(argv):
                 'https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/1',
                 trainable=FLAGS.fine_tune,
                 output_shape=[1280],
-                )
+            )
             self.dense = K.layers.Dense(units=3)
             self.softmax = K.layers.Softmax()
         
@@ -149,7 +212,7 @@ def main(argv):
     # Create training operations
     loss_func = tf.losses.CategoricalCrossentropy()
     optimizer = tf.optimizers.Adam(FLAGS.learning_rate)
-    train_accuracy = tf.metrics.Accuracy()
+    train_accuracy = tf.metrics.Accuracy(name='train_accuracy')
     train_loss = tf.metrics.Mean()
 
     @tf.function
@@ -157,7 +220,7 @@ def main(argv):
         with tf.GradientTape() as tape:
             logits = model(images)
             loss = loss_func(tf.one_hot(labels, 3), logits)
-        
+
         gradients = tape.gradient(loss, model.trainable_variables)
         optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
@@ -169,7 +232,7 @@ def main(argv):
     # Create a metric to compute the accuracy on the validation set 
 
     valid_loss = tf.metrics.Mean()
-    valid_accuracy = tf.metrics.Accuracy()
+    valid_accuracy = tf.metrics.Accuracy(name='valid_accuracy')
 
     @tf.function
     def valid_step(images, labels):
@@ -182,7 +245,7 @@ def main(argv):
         valid_accuracy.update_state(predictions, labels)
 
     test_loss = tf.metrics.Mean()
-    test_accuracy = tf.metrics.Accuracy()
+    test_accuracy = tf.metrics.Accuracy(name='test_accuracy')
 
     @tf.function
     def test_step(images, labels):
@@ -194,7 +257,7 @@ def main(argv):
         predictions = tf.argmax(logits, axis=-1, output_type=tf.int32)
         test_accuracy.update_state(predictions, labels)
     
-    # ============================= Train the model ================================
+    # =========================== Train the model ==============================
 
     # training
 
@@ -220,7 +283,7 @@ def main(argv):
 
         if step % FLAGS.valid_freq == 0:
             
-            for examples in valid_dataset:
+            for example in valid_dataset:
                 images, labels = example['image'], example['label']
                 valid_step(images, labels)    
 
@@ -233,13 +296,14 @@ def main(argv):
 
     # validation
 
-    for examples in valid_dataset:
+    for example in test_dataset:
         images, labels = example['image'], example['label']
         test_step(images, labels)    
 
     template = 'Test: loss = {:5.2f},  accuracy {:5.2f}%'
     logging.info(
         template.format(test_loss.result(), test_accuracy.result()*100))
-        
+
+
 if __name__ == '__main__':
     app.run(main)
